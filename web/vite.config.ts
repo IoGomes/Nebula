298f17bdca6e69be81cdb57c5a453784dfb02540
Esdras Carneiro <esdras.aslc2@gmail.com>
2025-11-21 12:32:48 -0300

    minor fix

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "./", // 🔥 ESSA LINHA RESOLVE SUA TELA BRANCA NO DIST
+  base: "./", 
 
   server: {
     host: "::",
